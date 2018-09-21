e17b9dcf783ecc6c1d82f79a7a734f79e6ed56fd
brae <bbraess@gmail.com>
2018-09-21 12:51:00 -0700

    added crossover

---
+++ GeneticAlgorithm/Crossover.cpp
@@ -0,0 +1,33 @@
+#include "MusicDS.h"
+#include "Crossover.h"
+#include <random>
+
+using namespace geneticalgorithm;
+using namespace music;
+
+std::random_device rd;
+std::mt19937 mt(rd());
+
+Chromosome geneticalgorithm::operators::crossover::cross(Chromosome parent1, Chromosome parent2) {
+	std::uniform_int_distribution<unsigned int> partDist(0, parent1.composition().numParts() - 1);
+	std::uniform_int_distribution<unsigned int> measureDist(0, parent1.composition().numMeasures() - 1);
+	unsigned int partIndex = partDist(mt);
+	unsigned int measureIndex = measureDist(mt);
+	Composition comp = parent1.composition();
+	Measure swapMeasure = parent2.composition().parts()[partIndex].measures()[measureIndex];
+	comp.replaceMeasureAt(partIndex, measureIndex, swapMeasure);
+	return Chromosome(comp);
+}
+
+std::vector<Chromosome> geneticalgorithm::operators::crossover::crossElites(std::vector<Chromosome> elites, Parameters params) {
+	std::vector<Chromosome> crossovers;
+	std::uniform_int_distribution<unsigned int> eliteDist(0, elites.size() - 1);
+	std::uniform_int_distribution<unsigned int> eliteDist2(0, elites.size() - 2);
+	for (int i = 0; i < params.numMutations; i++) {
+		unsigned int parent1Index = eliteDist(mt);
+		unsigned int parent2Index = eliteDist2(mt);
+		if (parent2Index >= parent1Index) parent2Index++;
+		crossovers.push_back(cross(elites[parent1Index], elites[parent2Index]));
+	}
+	return crossovers;
+}
--- GeneticAlgorithm/Crossover.h
+++ GeneticAlgorithm/Crossover.h
@@ -1,10 +1,13 @@
 #pragma once
 #include "Chromosome.h"
+#include "Parameters.h"
+#include <vector>
 
 namespace geneticalgorithm {
 	namespace operators {
 		namespace crossover {
-			void cross(Chromosome parent1, Chromosome parent2, Chromosome *child);
+			Chromosome cross(Chromosome parent1, Chromosome parent2);
+			std::vector<Chromosome> crossElites(std::vector<Chromosome> elites, Parameters params);
 		}
 	}
 }
