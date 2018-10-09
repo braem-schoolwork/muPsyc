4f51cc25a3fa6bc1a4f2599d636665b4b4c4be99
brae <bbraess@gmail.com>
2018-10-09 14:42:26 -0700

    included universals fitnesses

--- GeneticAlgorithm/include/FitnessInfo.h
+++ GeneticAlgorithm/include/FitnessInfo.h
@@ -5,8 +5,10 @@ namespace geneticalgorithm {
 		class FitnessInfo {
 		public:
 			double fitness;
+			double huron2001Fitness;
+			double brownJordana2011Fitness;
 
-			unsigned int numHuronFitnessRules = 13;
+			unsigned int numHuron2001FitnessRules = 13;
 			double registralCompassFitness;
 			double leapLengtheningFitness;
 			double partCrossingFitness;
@@ -21,11 +23,32 @@ namespace geneticalgorithm {
 			double avoidDisjunctApproachToFusedIntervalsFitness;
 			double chordSpacingFitness;
 
+			unsigned int numBrownJordana2011FitnessRules = 4;
+			double largeLeapResolutionFitness;
+			double unequalIntevalsFitness;
+			double scale7orLessDegreesFitness;
+			double limitedDurationValuesFitness;
+			double contourFitness;
+
+			void setHuron2001Fitness() {
+				huron2001Fitness = (registralCompassFitness + leapLengtheningFitness + partCrossingFitness +
+					pitchOverlappingFitness + semblantMotionFitness + parallelMotionFitness + avoidSemblantApproachBetweenFusedIntervalsFitness +
+					exposedIntervalsFitness + fusedIntervalsFitness + avoidTonalFusionFitness + obliqueApproachToFusedIntervalsFitness +
+					avoidDisjunctApproachToFusedIntervalsFitness + chordSpacingFitness) / static_cast<double>(numHuron2001FitnessRules);
+				fitness = huron2001Fitness;
+			}
+			void setBrownJordana2011Fitness() {
+				brownJordana2011Fitness = (largeLeapResolutionFitness + unequalIntevalsFitness + scale7orLessDegreesFitness +
+					limitedDurationValuesFitness) / static_cast<double>(numBrownJordana2011FitnessRules);
+				fitness = brownJordana2011Fitness;
+			}
 			void setOverallFitness() {
 				fitness = (registralCompassFitness + leapLengtheningFitness + partCrossingFitness +
 					pitchOverlappingFitness + semblantMotionFitness + parallelMotionFitness + avoidSemblantApproachBetweenFusedIntervalsFitness +
 					exposedIntervalsFitness + fusedIntervalsFitness + avoidTonalFusionFitness + obliqueApproachToFusedIntervalsFitness +
-					avoidDisjunctApproachToFusedIntervalsFitness + chordSpacingFitness) / static_cast<double>(numHuronFitnessRules);
+					avoidDisjunctApproachToFusedIntervalsFitness + chordSpacingFitness + largeLeapResolutionFitness + unequalIntevalsFitness +
+					scale7orLessDegreesFitness + limitedDurationValuesFitness)
+					/ static_cast<double>(numHuron2001FitnessRules + numBrownJordana2011FitnessRules);
 			}
 
 			FitnessInfo() {}
