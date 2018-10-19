732e8c2da680c06afed142f94fac8529bb087460
brae <bbraess@gmail.com>
2018-10-19 15:45:45 -0700

    implemented new mutation ops

--- GeneticAlgorithm/include/Mutation.h
+++ GeneticAlgorithm/include/Mutation.h
@@ -17,36 +17,47 @@ namespace geneticalgorithm {
 						unsigned int *partIndex, unsigned int *measureIndex, unsigned int *noteIndex);
 					void getRandomNoteIndices(Chromosome chromosome,
 						unsigned int *partIndex, unsigned int *measureIndex, unsigned int *noteIndex1, unsigned int *noteIndex2);
+					void getRandomNoteCoupleIndices(Chromosome chromosome,
+						unsigned int *partIndex, unsigned int *measureIndex, unsigned int *noteIndex1, unsigned int *noteIndex2);
 					void transposeRandomNote(Chromosome *chromosome, unsigned int degree);
 				}
-				//single note mutations. f:N->N
-				Chromosome leap(Chromosome chromosome);
-				Chromosome lowerNeighbor(Chromosome chromosome);
-				Chromosome upperNeighbor(Chromosome chromosome);
-				Chromosome arpeggiate(Chromosome chromosome);
-				//f:N->NxN
+				namespace old {
+					//single note mutations. f:N->N
+					Chromosome leap(Chromosome chromosome);
+					Chromosome lowerNeighbor(Chromosome chromosome);
+					Chromosome upperNeighbor(Chromosome chromosome);
+					Chromosome arpeggiate(Chromosome chromosome);
+					//f:N->NxN
+					Chromosome split(Chromosome chromosome);
+					Chromosome anticipation(Chromosome chromosome);
+					Chromosome delay(Chromosome chromosome);
+					//f:NxN->N
+					Chromosome merge(Chromosome chromosome);
+					Chromosome removeNote(Chromosome chromosome);
+					//f:NxN -> NxNxN
+					Chromosome passingTone(Chromosome chromosome);
+					Chromosome forceStepwise(Chromosome chromosome);
+					//sub operator indices
+					unsigned const int soi_leap = 0;
+					unsigned const int soi_lowerNeighbor = 1;
+					unsigned const int soi_upperNeighbor = 2;
+					unsigned const int soi_arpeggiate = 3;
+					unsigned const int soi_split = 4;
+					unsigned const int soi_anticipation = 5;
+					unsigned const int soi_delay = 6;
+					unsigned const int soi_merge = 7;
+					unsigned const int soi_removeNote = 8;
+					unsigned const int soi_passingTone = 9;
+					unsigned const int soi_forceStepwise = 10;
+				}
+
+				Chromosome randomTranspose(Chromosome chromosome);
 				Chromosome split(Chromosome chromosome);
-				Chromosome anticipation(Chromosome chromosome);
-				Chromosome delay(Chromosome chromosome);
-				//f:NxN->N
 				Chromosome merge(Chromosome chromosome);
-				Chromosome removeNote(Chromosome chromosome);
-				//f:NxN -> NxNxN
-				Chromosome passingTone(Chromosome chromosome);
-				Chromosome forceStepwise(Chromosome chromosome);
+				unsigned int soi_randomTranspose = 0;
+				unsigned int soi_split = 1;
+				unsigned int soi_merge = 2;
 			}
-			//sub operator indices
-			unsigned const int soi_leap = 0;
-			unsigned const int soi_lowerNeighbor = 1;
-			unsigned const int soi_upperNeighbor = 2;
-			unsigned const int soi_arpeggiate = 3;
-			unsigned const int soi_split = 4;
-			unsigned const int soi_anticipation = 5;
-			unsigned const int soi_delay = 6;
-			unsigned const int soi_merge = 7;
-			unsigned const int soi_removeNote = 8;
-			unsigned const int soi_passingTone = 9;
-			unsigned const int soi_forceStepwise = 10;
 			Chromosome mutate(Chromosome chromosome, std::vector<double> operatorProbabilities);
 			std::vector<Chromosome> mutateElites(std::vector<Chromosome> elites, Parameters params);
 		}
--- GeneticAlgorithm/source/Mutation.cpp
+++ GeneticAlgorithm/source/Mutation.cpp
@@ -8,7 +8,7 @@
 using namespace geneticalgorithm;
 using namespace music;
 
-Chromosome geneticalgorithm::operators::mutation::sub::leap(Chromosome chromosome) {
+Chromosome geneticalgorithm::operators::mutation::sub::old::leap(Chromosome chromosome) {
 	Chromosome newChromosome = Chromosome(chromosome);
 	std::uniform_int_distribution<int> leapDist(INT_2ND, INT_6TH);
 	if (boolDist(mt)) //up
@@ -18,19 +18,19 @@ Chromosome geneticalgorithm::operators::mutation::sub::leap(Chromosome chromosom
 	return newChromosome;
 }
 
-Chromosome geneticalgorithm::operators::mutation::sub::lowerNeighbor(Chromosome chromosome) {
+Chromosome geneticalgorithm::operators::mutation::sub::old::lowerNeighbor(Chromosome chromosome) {
 	Chromosome newChromosome = Chromosome(chromosome);
 	helper::transposeRandomNote(&newChromosome, -INT_2ND);
 	return newChromosome;
 }
 
-Chromosome geneticalgorithm::operators::mutation::sub::upperNeighbor(Chromosome chromosome) {
+Chromosome geneticalgorithm::operators::mutation::sub::old::upperNeighbor(Chromosome chromosome) {
 	Chromosome newChromosome = Chromosome(chromosome);
 	helper::transposeRandomNote(&newChromosome, INT_2ND);
 	return newChromosome;
 }
 
-Chromosome geneticalgorithm::operators::mutation::sub::arpeggiate(Chromosome chromosome) {
+Chromosome geneticalgorithm::operators::mutation::sub::old::arpeggiate(Chromosome chromosome) {
 	Chromosome newChromosome = Chromosome(chromosome);
 	bool direction = boolDist(mt);
 	bool type = boolDist(mt); //3rd or 5th
@@ -42,7 +42,7 @@ Chromosome geneticalgorithm::operators::mutation::sub::arpeggiate(Chromosome chr
 	return newChromosome;
 }
 
-Chromosome geneticalgorithm::operators::mutation::sub::split(Chromosome chromosome) {
+Chromosome geneticalgorithm::operators::mutation::sub::old::split(Chromosome chromosome) {
 	unsigned int partIndex, measureIndex, noteIndex; Composition comp = chromosome.composition();
 	helper::getRandomNoteIndex(chromosome, &partIndex, &measureIndex, &noteIndex);
 	Note selectedNote = comp.parts()[partIndex].measures()[measureIndex].notes()[noteIndex];
@@ -63,7 +63,7 @@ Chromosome geneticalgorithm::operators::mutation::sub::split(Chromosome chromoso
 	return Chromosome(comp);
 }
 
-Chromosome geneticalgorithm::operators::mutation::sub::anticipation(Chromosome chromosome) {
+Chromosome geneticalgorithm::operators::mutation::sub::old::anticipation(Chromosome chromosome) {
 	unsigned int partIndex, measureIndex, noteIndex; Composition comp = chromosome.composition();
 	helper::getRandomNoteIndex(chromosome, &partIndex, &measureIndex, &noteIndex);
 	Note selectedNote = comp.parts()[partIndex].measures()[measureIndex].notes()[noteIndex];
@@ -85,7 +85,7 @@ Chromosome geneticalgorithm::operators::mutation::sub::anticipation(Chromosome c
 	return Chromosome(comp);
 }
 
-Chromosome geneticalgorithm::operators::mutation::sub::delay(Chromosome chromosome) {
+Chromosome geneticalgorithm::operators::mutation::sub::old::delay(Chromosome chromosome) {
 	unsigned int partIndex, measureIndex, noteIndex; Composition comp = chromosome.composition();
 	helper::getRandomNoteIndex(chromosome, &partIndex, &measureIndex, &noteIndex);
 	Note selectedNote = comp.parts()[partIndex].measures()[measureIndex].notes()[noteIndex];
@@ -107,7 +107,7 @@ Chromosome geneticalgorithm::operators::mutation::sub::delay(Chromosome chromoso
 	return Chromosome(comp);
 }
 
-Chromosome geneticalgorithm::operators::mutation::sub::merge(Chromosome chromosome) {
+Chromosome geneticalgorithm::operators::mutation::sub::old::merge(Chromosome chromosome) {
 	unsigned int partIndex, measureIndex, noteIndex1, noteIndex2; Composition comp = chromosome.composition();
 	helper::getRandomNoteIndices(chromosome, &partIndex, &measureIndex, &noteIndex1, &noteIndex2);
 	Note selectedNote1 = comp.parts()[partIndex].measures()[measureIndex].notes()[noteIndex1];
@@ -131,7 +131,7 @@ Chromosome geneticalgorithm::operators::mutation::sub::merge(Chromosome chromoso
 	return Chromosome(comp);
 }
 
-Chromosome geneticalgorithm::operators::mutation::sub::removeNote(Chromosome chromosome) {
+Chromosome geneticalgorithm::operators::mutation::sub::old::removeNote(Chromosome chromosome) {
 	unsigned int partIndex, measureIndex, noteIndex1, noteIndex2; Composition comp = chromosome.composition();
 	helper::getRandomNoteIndices(chromosome, &partIndex, &measureIndex, &noteIndex1, &noteIndex2);
 	bool b = boolDist(mt);
@@ -155,7 +155,7 @@ Chromosome geneticalgorithm::operators::mutation::sub::removeNote(Chromosome chr
 	return Chromosome(comp);
 }
 
-Chromosome geneticalgorithm::operators::mutation::sub::passingTone(Chromosome chromosome) {
+Chromosome geneticalgorithm::operators::mutation::sub::old::passingTone(Chromosome chromosome) {
 	unsigned int partIndex, measureIndex, noteIndex1, noteIndex2; Composition comp = chromosome.composition();
 	helper::getRandomNoteIndices(chromosome, &partIndex, &measureIndex, &noteIndex1, &noteIndex2);
 	bool b = boolDist(mt);
@@ -173,7 +173,7 @@ Chromosome geneticalgorithm::operators::mutation::sub::passingTone(Chromosome ch
 	return Chromosome(comp);
 }
 
-Chromosome geneticalgorithm::operators::mutation::sub::forceStepwise(Chromosome chromosome) {
+Chromosome geneticalgorithm::operators::mutation::sub::old::forceStepwise(Chromosome chromosome) {
 	unsigned int partIndex, measureIndex, noteIndex1, noteIndex2; Composition comp = chromosome.composition();
 	helper::getRandomNoteIndices(chromosome, &partIndex, &measureIndex, &noteIndex1, &noteIndex2);
 	bool up = boolDist(mt), b = boolDist(mt), which = boolDist(mt);
@@ -236,6 +236,25 @@ void geneticalgorithm::operators::mutation::sub::helper::getRandomNoteIndices(Ch
 	*noteIndex2 = *noteIndex1 + 1;
 }
 
+void geneticalgorithm::operators::mutation::sub::helper::getRandomNoteCoupleIndices(Chromosome chromosome, unsigned int * partIndex, unsigned int * measureIndex, unsigned int * noteIndex1, unsigned int * noteIndex2) {
+	std::uniform_int_distribution<int> partDist(0, chromosome.composition().numParts() - 1);
+	std::uniform_int_distribution<int> measureDist(0, chromosome.composition().numMeasures() - 1);
+	*partIndex = partDist(mt);
+	*measureIndex = measureDist(mt);
+	std::vector<Note> notes = chromosome.composition().parts()[*partIndex].measures()[*measureIndex].notes();
+	if (notes.size() <= 1) {
+		*noteIndex1 = -1; *noteIndex2 = -1; return;
+	}
+	std::vector<unsigned int> possibleIndices;
+	for (unsigned int i = 1; i < notes.size(); i++) {
+		if (notes[i - 1].duration() == notes[i].duration())
+			possibleIndices.push_back(i - 1);
+	}
+	std::uniform_int_distribution<int> noteDist(0, possibleIndices.size());
+	*noteIndex1 = possibleIndices[noteDist(mt)];
+	*noteIndex2 = *noteIndex1 + 1;
+}
+
 void geneticalgorithm::operators::mutation::sub::helper::transposeRandomNote(Chromosome * chromosome, unsigned int degree) {
 	unsigned int partIndex, measureIndex, noteIndex; Composition comp = chromosome->composition();
 	getRandomNoteIndex(*chromosome, &partIndex, &measureIndex, &noteIndex);
@@ -249,17 +268,9 @@ void geneticalgorithm::operators::mutation::sub::helper::transposeRandomNote(Chr
 Chromosome geneticalgorithm::operators::mutation::mutate(Chromosome chromosome, std::vector<double> operatorProbabilities) {
 	unsigned int operatorIndex = algorithm::roulleteSelect(operatorProbabilities);
 	switch (operatorIndex) {
-	case soi_leap: return sub::leap(chromosome);
-	case soi_lowerNeighbor: return sub::lowerNeighbor(chromosome);
-	case soi_upperNeighbor: return sub::upperNeighbor(chromosome);
-	case soi_arpeggiate: return sub::arpeggiate(chromosome);
-	case soi_split: return sub::split(chromosome);
-	case soi_anticipation: return sub::anticipation(chromosome);
-	case soi_delay: return sub::delay(chromosome);
-	case soi_merge: return sub::merge(chromosome);
-	case soi_removeNote: return sub::removeNote(chromosome);
-	case soi_passingTone: return sub::passingTone(chromosome);
-	case soi_forceStepwise: return sub::forceStepwise(chromosome);
+	case 0: return sub::randomTranspose(chromosome);
+	case 1: return sub::split(chromosome);
+	case 2: return sub::merge(chromosome);
 	default: throw std::invalid_argument("Operator Index Out of Range");
 	}
 }
@@ -267,12 +278,48 @@ Chromosome geneticalgorithm::operators::mutation::mutate(Chromosome chromosome, 
 std::vector<Chromosome> geneticalgorithm::operators::mutation::mutateElites(std::vector<Chromosome> elites, Parameters params) {
 	std::vector<Chromosome> mutations;
 	std::uniform_int_distribution<int> eliteDist(0, elites.size() - 1);
-	std::vector<double> operatorProbs = { params.op_leap, params.op_lowerNeighbor, params.op_upperNeighbor, params.op_arpeggiate,
-				params.op_split, params.op_anticipation, params.op_delay, params.op_merge, params.op_removeNote,
-				params.op_passingTone, params.op_forceStepwise };
+	std::vector<double> operatorProbs = { params.op_randomTranspose, params.op_split, params.op_merge };
 	for (int i = 0; i < params.numMutations; i++)
 		mutations.push_back(
 			mutate(elites[eliteDist(mt)], operatorProbs )
 		);
 	return mutations;
+}
+
+Chromosome geneticalgorithm::operators::mutation::sub::randomTranspose(Chromosome chromosome) {
+	Chromosome newChromosome = Chromosome(chromosome);
+	std::uniform_int_distribution<int> leapDist(INT_UNISON, INT_6TH);
+	if (boolDist(mt)) helper::transposeRandomNote(&newChromosome, leapDist(mt));
+	else helper::transposeRandomNote(&newChromosome, 0 - leapDist(mt));
+	return newChromosome;
+}
+
+Chromosome geneticalgorithm::operators::mutation::sub::split(Chromosome chromosome) {
+	unsigned int partIndex, measureIndex, noteIndex; Composition comp = chromosome.composition();
+	helper::getRandomNoteIndex(chromosome, &partIndex, &measureIndex, &noteIndex);
+	Note selectedNote = comp.parts()[partIndex].measures()[measureIndex].notes()[noteIndex];
+	//2nd note doesnt carry over the ties
+	Note newNote1 = Note(selectedNote), newNote2 = Note(selectedNote.pitch(), selectedNote.duration());
+	Duration d = selectedNote.duration();
+	d.halfDuration();
+	newNote1.setDuration(d); newNote2.setDuration(d);
+	comp.replaceNoteAt(partIndex, measureIndex, noteIndex, newNote1);
+	comp.addNoteAt(partIndex, measureIndex, noteIndex, newNote2);
+	return Chromosome(comp);
+}
+
+Chromosome geneticalgorithm::operators::mutation::sub::merge(Chromosome chromosome) {
+	unsigned int partIndex, measureIndex, noteIndex1, noteIndex2; Composition comp = chromosome.composition();
+	helper::getRandomNoteCoupleIndices(chromosome, &partIndex, &measureIndex, &noteIndex1, &noteIndex2);
+	Note selectedNote1 = comp.parts()[partIndex].measures()[measureIndex].notes()[noteIndex1];
+	Note selectedNote2 = comp.parts()[partIndex].measures()[measureIndex].notes()[noteIndex2];
+	Key key = comp.parts()[partIndex].measures()[measureIndex].key();
+	Pitch newPitch; Duration newDuration;
+	key.meanPitch(selectedNote1.pitch(), selectedNote2.pitch(), &newPitch);
+	if (Duration::add(selectedNote1.duration(), selectedNote2.duration(), &newDuration)) {
+		Note newNote = Note(newPitch, newDuration);
+		comp.replaceNoteAt(partIndex, measureIndex, noteIndex1, newNote);
+		comp.removeNoteAt(partIndex, measureIndex, noteIndex2);
+	}
+	return Chromosome(comp);
 }
