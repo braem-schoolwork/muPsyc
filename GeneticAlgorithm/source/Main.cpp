95383bfbf81d4b614ea36595759491f5c7e78f1e
brae <bbraess@gmail.com>
2018-10-19 17:46:23 -0700

    updated to new changes

--- GeneticAlgorithm/source/Main.cpp
+++ GeneticAlgorithm/source/Main.cpp
@@ -24,18 +24,18 @@ int main() {
 	initParams.partNames = { "Part1", "Part2", "Part3", "Part4" };
 	char instr[4] = { ACOUSTICGRANDPIANO, ACOUSTICGRANDPIANO, ACOUSTICGRANDPIANO, ACOUSTICGRANDPIANO };
 	initParams.instruments = instr;
-	initParams.key = GMAJKEY;
+	initParams.key = CMAJKEY;
 	initParams.timeSig = TimeSignature(4, 4);
 	initParams.bpm = BPM(60, 4);
 	initParams.numParts = 4;
 	initParams.numMeasures = 4;
-	initParams.lowerBounds = { C4, C4, C4, C4 };
-	initParams.upperBounds = { C6, C6, C6, C6 };
+	initParams.lowerBounds = { C2, C3, G3, C4 };
+	initParams.upperBounds = { C4, G4, C5, D5 };
 
 	Parameters params;
 	params.initParams = initParams;
 	params.populationSize = 60;
-	params.numGenerations = 2;
+	params.numGenerations = 50;
 
 	params.numElites = 10;
 	params.numMutations = 25;
@@ -44,6 +44,11 @@ int main() {
 	params.onsetSyncLowerBound = 0.3;
 	params.onsetSyncUpperBound = 0.6;
 	
+	params.op_randomTranspose = 0.4;
+	params.op_split = 0.25;
+	params.op_merge = 0.25;
+	params.op_repeat = 0.1;
+
 	params.op_old_leap = 0.125;
 	params.op_old_upperNeighbor = 0.0625;
 	params.op_old_lowerNeighbor = 0.0625;
@@ -58,9 +63,7 @@ int main() {
 
 	RunInfo runInfo = runGA(params);
 
-	IO::writeCompositionToMIDI("test.mid", runInfo.best.composition());
-
-	Composition comp = IO::readMIDI("test.mid");
+	IO::writeCompositionToMIDI("output.mid", runInfo.best.composition());
 
 	return 0;
 }
