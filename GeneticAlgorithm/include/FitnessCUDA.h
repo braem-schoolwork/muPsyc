b9951543545439c6d14d1c1c01fb2f84fa34f2ca
brae <bbraess@gmail.com>
2018-10-26 18:28:46 -0700

    still doesnt work. need to make a lot of device only funcs

--- GeneticAlgorithm/include/FitnessCUDA.h
+++ GeneticAlgorithm/include/FitnessCUDA.h
@@ -1,8 +1,9 @@
 #pragma once
 #include "Population.h"
 #include "Parameters.h"
+#include "Fitness.h"
 //#include <thrust/copy.h>
 
 namespace geneticalgorithm {
-	void evaluateWithCUDA(Population *population, Parameters params);
+	void evaluateWithCUDA(Population &population, Parameters params);
 }
