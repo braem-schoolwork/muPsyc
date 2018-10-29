62eb218826698e9c242ff072b733d409ff158c8d
brae <bbraess@gmail.com>
2018-10-29 13:46:40 -0700

    deleted cuda files

--- GeneticAlgorithm/include/FitnessCUDA.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "Population.h"
-#include "Parameters.h"
-#include "Fitness.h"
-//#include <thrust/copy.h>
-
-namespace geneticalgorithm {
-	void evaluateWithCUDA(Population &population, Parameters params);
-}
