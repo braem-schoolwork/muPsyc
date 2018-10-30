8287c7c5984032de519f568848a68d53de3d9f39
brae <bbraess@gmail.com>
2018-10-30 10:35:12 -0700

    added more types

--- GeneticAlgorithm/include/SelectionType.h
+++ GeneticAlgorithm/include/SelectionType.h
@@ -2,7 +2,7 @@
 namespace geneticalgorithm {
 	namespace operators {
 		namespace selection {
-			enum SelectionType { ROULETTE_WHEEL, SURVIVAL_OF_THE_FITTEST };
+			enum SelectionType { ROULETTE_WHEEL, FITNESS_PROPORTIONATE, RANK, TOURNAMENT };
 		}
 	}
 }
