eaadb78542808ba3c9f5dd568bbc3188bef5dc77
brae <bbraess@gmail.com>
2018-10-30 09:59:14 -0700

    fixed issue when numCross =/= numMut

--- GeneticAlgorithm/source/Crossover.cpp
+++ GeneticAlgorithm/source/Crossover.cpp
@@ -18,14 +18,14 @@ Chromosome geneticalgorithm::operators::crossover::cross(Chromosome parent1, Chr
 }
 
 std::vector<Chromosome> geneticalgorithm::operators::crossover::crossElites(std::vector<Chromosome> elites, Parameters params) {
-	std::vector<Chromosome> crossovers;
+	std::vector<Chromosome> crossovers(params.numCrossovers);
 	std::uniform_int_distribution<unsigned int> eliteDist(0, elites.size() - 1);
 	std::uniform_int_distribution<unsigned int> eliteDist2(0, elites.size() - 2);
-	for (int i = 0; i < params.numMutations; i++) {
+	for (int i = 0; i < params.numCrossovers; i++) {
 		unsigned int parent1Index = eliteDist(mt);
 		unsigned int parent2Index = eliteDist2(mt);
 		if (parent2Index >= parent1Index) parent2Index++;
-		crossovers.push_back(cross(elites[parent1Index], elites[parent2Index]));
+		crossovers[i] = cross(elites[parent1Index], elites[parent2Index]);
 	}
 	return crossovers;
 }
