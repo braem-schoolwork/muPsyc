33a2953bd9490a5deaf2a234703b06febb57a1ca
brae <bbraess@gmail.com>
2018-11-05 12:34:27 -0800

    added an include all file

---
+++ GeneticAlgorithm/include/MusicGA.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "MusicDS.h"
+#include <omp.h>
+#include <random>
+
+#include "SelectionType.h"
+#include "OptimizationType.h"
+#include "FitnessScalingType.h"
+
+#include "Chromosome.h"
+#include "Population.h"
+#include "InitParams.h"
+#include "Parameters.h"
+
+#include "CompositionGenerator.h"
+
+#include "FitnessInfo.h"
+#include "Fitness.h"
+#include "Operators.h"
+#include "Selection.h"
+#include "Mutation.h"
+#include "Crossover.h"
+
+#include "RunInfo.h"
+#include "GeneticAlgorithm.h"
