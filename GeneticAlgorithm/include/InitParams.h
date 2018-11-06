db058604f1397249ddb108872b67e3b69c7a5e14
brae <bbraess@gmail.com>
2018-11-06 12:03:09 -0800

    moved operator<< into class as a friend

--- GeneticAlgorithm/include/InitParams.h
+++ GeneticAlgorithm/include/InitParams.h
@@ -19,34 +19,35 @@ namespace geneticalgorithm {
 			std::vector<music::Pitch> upperBounds{ music::C4, music::G4, music::C5, music::D5 };
 
 			InitParams() {}
+
+			friend std::ostream& operator<<(std::ostream &strm, const InitParams & iParams) {
+				strm << "Composition Name: " << iParams.name << std::endl;
+				strm << "Number of Parts: " << iParams.numParts << std::endl;
+				strm << "Number of Measures: " << iParams.numMeasures << std::endl;
+				strm << "Part Names: ";
+				for (size_t i = 0; i < iParams.partNames.size(); i++) {
+					strm << iParams.partNames[i];
+					if (i == iParams.partNames.size() - 1)
+						strm << ", ";
+					else strm << std::endl;
+				}
+				strm << "Key: " << iParams.key << std::endl;
+				strm << "Time Signature: " << iParams.timeSig << std::endl;
+				strm << "BPM: " << iParams.bpm << std::endl;
+				strm << "Instruments: ";
+				for (size_t i = 0; i < iParams.instruments.size(); i++) {
+					strm << iParams.instruments[i];
+					if (i == iParams.instruments.size() - 1)
+						strm << ", ";
+					else strm << std::endl;
+				}
+				strm << "Bounds:" << std::endl;
+				for (size_t i = 0; i < iParams.lowerBounds.size(); i++) {
+					strm << "\t Part " << i << "--- Lower: " << iParams.lowerBounds[i] <<
+						", Upper: " << iParams.upperBounds[i] << std::endl;
+				}
+				return strm;
+			}
 		};
-		std::ostream& operator<<(std::ostream &strm, const InitParams & iParams) {
-			strm << "Composition Name: " << iParams.name << std::endl;
-			strm << "Number of Parts: " << iParams.numParts << std::endl;
-			strm << "Number of Measures: " << iParams.numMeasures << std::endl;
-			strm << "Part Names: ";
-			for (size_t i = 0; i < iParams.partNames.size(); i++) {
-				strm << iParams.partNames[i];
-				if (i == iParams.partNames.size() - 1)
-					strm << ", ";
-				else strm << std::endl;
-			}
-			strm << "Key: " << iParams.key << std::endl;
-			strm << "Time Signature: " << iParams.timeSig << std::endl;
-			strm << "BPM: " << iParams.bpm << std::endl;
-			strm << "Instruments: ";
-			for (size_t i = 0; i < iParams.instruments.size(); i++) {
-				strm << iParams.instruments[i];
-				if (i == iParams.instruments.size() - 1)
-					strm << ", ";
-				else strm << std::endl;
-			}
-			strm << "Bounds:" << std::endl;
-			for (size_t i = 0; i < iParams.lowerBounds.size(); i++) {
-				strm << "\t Part " << i << "--- Lower: " << iParams.lowerBounds[i] << 
-					", Upper: " << iParams.upperBounds[i] << std::endl;
-			}
-			return strm;
-		}
 	}
 }
--- GeneticAlgorithm/include/Parameters.h
+++ GeneticAlgorithm/include/Parameters.h
@@ -75,38 +75,55 @@ namespace geneticalgorithm {
 		}
 		
 		Parameters() {}
-	};
-	std::ostream& operator<<(std::ostream &strm, const Parameters params) {
-		strm << params.initParams << std::endl;
-		strm << "Population Size: " << params.populationSize << std::endl;
-		strm << "Number of Generations: " << params.numGenerations << std::endl;
-		strm << "Number of Elites: " << params.numElites << std::endl;
-		strm << "Number of Mutations: " << params.numMutations << std::endl;
-		strm << "Number of Crossovers: " << params.numCrossovers << std::endl;
-		strm << "Elitism Size: " << params.elitismCount << std::endl;
-		strm << "Selection Method: " << params.selType << std::endl;
-		if (params.selType == operators::selection::TOURNAMENT_DETERMINISTIC)
-			strm << "Tournament Size: " << params.tournamentSize << std::endl;
-		if (params.selType == operators::selection::TOURNAMENT) {
-			strm << "Tournament Size: " << params.tournamentSize << std::endl;
-			strm << "Tournament Selection Probability: " << params.tournamentProb << std::endl;
-		}
-		strm << "Fitness Scaling Method: " << params.fitnessScalingType << std::endl;
-		if (params.fitnessScalingType == fitness::POWER_LAW)
-			strm << "Power Law Power: " << params.powerLawScalingPower << std::endl;
-		strm << "Mutation Operator Probabilities: " << std::endl;
-		strm << "\t Random Transposition: " << params.op_randomTranspose << std::endl;
-		strm << "\t Split: " << params.op_split << std::endl;
-		strm << "\t Merge: " << params.op_merge << std::endl;
-		strm << "\t Repeat: " << params.op_repeat << std::endl;
-		strm << "Optimization: " << std::endl;
-		strm << "\t Fitness Function: " << params.fitnessOptType << std::endl;
-		strm << "\t Selection: " << params.selOptType << std::endl;
-		strm << "\t Mutation: " << params.mutOptType << std::endl;
-		strm << "\t Crossover: " << params.crossOptType << std::endl;
-		return strm;
-	}
-	std::ostream& operator>>(std::ostream &strm, const Parameters params) {
 
-	}
+
+		friend std::ostream& operator<<(std::ostream &strm, const Parameters & params) {
+			strm << params.initParams << std::endl;
+			strm << "Population Size: " << params.populationSize << std::endl;
+			strm << "Number of Generations: " << params.numGenerations << std::endl;
+			strm << "Number of Elites: " << params.numElites << std::endl;
+			strm << "Number of Mutations: " << params.numMutations << std::endl;
+			strm << "Number of Crossovers: " << params.numCrossovers << std::endl;
+			strm << "Elitism Size: " << params.elitismCount << std::endl;
+			strm << "Selection Method: " << params.selType << std::endl;
+			if (params.selType == operators::selection::TOURNAMENT_DETERMINISTIC)
+				strm << "Tournament Size: " << params.tournamentSize << std::endl;
+			if (params.selType == operators::selection::TOURNAMENT) {
+				strm << "Tournament Size: " << params.tournamentSize << std::endl;
+				strm << "Tournament Selection Probability: " << params.tournamentProb << std::endl;
+			}
+			strm << "Fitness Scaling Method: " << params.fitnessScalingType << std::endl;
+			if (params.fitnessScalingType == fitness::POWER_LAW)
+				strm << "Power Law Power: " << params.powerLawScalingPower << std::endl;
+			strm << "Mutation Operator Probabilities: " << std::endl;
+			strm << "\t Random Transposition: " << params.op_randomTranspose << std::endl;
+			strm << "\t Split: " << params.op_split << std::endl;
+			strm << "\t Merge: " << params.op_merge << std::endl;
+			strm << "\t Repeat: " << params.op_repeat << std::endl;
+			strm << "Optimization: " << std::endl;
+			strm << "\t Fitness Function: " << params.fitnessOptType << std::endl;
+			strm << "\t Selection: " << params.selOptType << std::endl;
+			strm << "\t Mutation: " << params.mutOptType << std::endl;
+			strm << "\t Crossover: " << params.crossOptType << std::endl;
+			return strm;
+		}
+		friend std::istream& operator>>(std::istream &strm, Parameters & params) {
+			/*
+			strm >> params.initParams >> params.populationSize >> params.numGenerations >> params.numElites >> params.numCrossovers
+				>> params.elitismCount >> params.selType;
+			if (params.selType == operators::selection::TOURNAMENT_DETERMINISTIC) {
+				strm >> params.tournamentSize;
+			}
+			else if (params.selType == operators::selection::TOURNAMENT) {
+				strm >> params.tournamentSize >> params.tournamentProb;
+			}
+			strm >> params.fitnessScalingType;
+			if (params.fitnessScalingType == fitness::POWER_LAW)
+				strm >> params.powerLawScalingPower;
+			strm >> params.op_randomTranspose >> params.op_split >> params.op_merge >> params.op_repeat
+				>> params.fitnessOptType >> params.selOptType >> params.mutOptType >> params.crossOptType;
+				*/
+			return strm;
+		}
+	};
 }
