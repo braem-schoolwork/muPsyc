94092427685ff5efd40b4381780ae3b1bdf04c89
Brae Stoltz <bbraess@gmail.com>
2018-11-07 20:25:38 -0800

    added a loader for the base config

--- Experimentation/source/Main.cpp
+++ Experimentation/source/Main.cpp
@@ -1,6 +1,11 @@
 #include "MusicGA.h"
+#include <fstream>
 
 int main() {
+	std::ifstream ifs;
+	ifs.open("config.cfg");
+	ifs >> geneticalgorithm::AlgorithmParameters;
+	ifs.close();
 
 	geneticalgorithm::runGA();
 
