e2e8d5a2434b6b9a6109ae365caea2a9c85d912f
brae <bbraess@gmail.com>
2019-02-06 11:14:52 -0800

    added age field

--- GeneticAlgorithm/include/Chromosome.h
+++ GeneticAlgorithm/include/Chromosome.h
@@ -7,18 +7,26 @@ namespace geneticalgorithm {
 	private:
 		music::Composition c;
 		fitness::FitnessInfo f;
+		unsigned int a;
 
 	public:
 		Chromosome() {}
-		Chromosome(music::Composition composition) : c(composition) {}
+		Chromosome(music::Composition composition) : c(composition), a(0) {}
 
 		music::Composition composition() const { return c; }
 		fitness::FitnessInfo fitnessInfo() const { return f; }
 		double fitness() const { return f.fitness; }
+		unsigned int age() const { return a; }
 
 		void setComposition(music::Composition composition) { c = composition; }
 		void setFitnessInfo(fitness::FitnessInfo fitnessInfo) { f = fitnessInfo; }
 		void setFitness(double fitness) { f.fitness = fitness; }
+		void setAge(unsigned int age) { a = age; }
+
+		void incrementAge() { a++; }
+		bool isOlder(Chromosome other) { return a > other.a; }
+		bool isYounger(Chromosome other) { return a < other.a; }
+		bool isSameAge(Chromosome other) { return a == other.a; }
 
 		bool operator==(const Chromosome & other) const {
 			double thisFit = this->fitness(), otherFit = other.fitness(), epsilon = std::numeric_limits<double>().epsilon();
