b42405f0fd9a3b834d7de8cd6499ad65aa49e078
Brae Stoltz <bbraess@gmail.com>
2019-02-20 13:13:56 -0800

    added constructor for pitches and notes

--- MusicDS/include/Chord.h
+++ MusicDS/include/Chord.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <vector>
+#include "Note.h"
+#include "Pitch.h"
 
 namespace music {
 	class Chord {
@@ -9,6 +11,12 @@ namespace music {
 	public:
 		Chord() {}
 		Chord(std::vector<unsigned int> pcs) : p(pcs) {}
+		Chord(std::vector<Pitch> pitches) {
+			for (Pitch pit : pitches) p.push_back(pit.pitchClass());
+		}
+		Chord(std::vector<Note> notes) {
+			for (Note note : notes) p.push_back(note.pitch().pitchClass());
+		}
 
 		std::vector<unsigned int> pcs() { return p; }
 		void setPCs(std::vector<unsigned int> pcs) { this->p = pcs; }
