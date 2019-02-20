6aee7a21e579c57ad0bbd866abd78c6062adf04b
Brae Stoltz <bbraess@gmail.com>
2019-02-20 11:38:55 -0800

    added a similarity function

--- MusicDS/include/Chord.cpp
+++ MusicDS/include/Chord.cpp
@@ -26,3 +26,32 @@ std::istream & music::operator>>(std::istream & strm, Chord & ch) {
 	ch = Chord(pcs);
 	return strm;
 }
+
+bool music::Chord::isSameFundamental(Chord other) {
+	return other.p[0] == this->p[0];
+}
+
+bool music::Chord::isTriad() {
+	return p.size() <= 3;
+}
+
+bool music::Chord::isSimilarChord(Chord other) {
+	bool has3rd = false, has5th = false, hasAux = false, bassOnFundamental = isSameFundamental(other), allInChord = true;
+	for (unsigned int otherpc : other.p) {
+		bool isInChord = false;
+		for (unsigned int thispc : this->p)
+			if (otherpc == thispc) { isInChord = true; break; }
+		if (!isInChord) allInChord = false;
+		if (otherpc == this->p[1]) has3rd = true;
+		if (otherpc == this->p[2]) has5th = true;
+		for (unsigned int i = 3; i < this->p.size(); i++)
+			if (otherpc == this->p[i]) hasAux = true;
+	}
+	if (this->isTriad()) {
+		if (bassOnFundamental && has3rd && has5th && allInChord) return true;
+	}
+	else {
+		if (bassOnFundamental && has3rd && hasAux && allInChord) return true;
+	}
+	return false;
+}
--- MusicDS/include/Chord.h
+++ MusicDS/include/Chord.h
@@ -16,6 +16,10 @@ namespace music {
 		unsigned int fundamental() { return p[0]; }
 		std::vector<unsigned int> triad() { return std::vector(p.begin(), p.begin() + 2); }
 
+		bool isSameFundamental(Chord other);
+		bool isTriad();
+		bool isSimilarChord(Chord other);
+
 		friend std::ostream& operator<<(std::ostream &strm, const Chord &chord);
 		//[0,2,4]
 		friend std::istream & operator >> (std::istream &strm, Chord &ch);
